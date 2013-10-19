70befa62991ab2b2153313242d0225e1ac232c1d
Joshua Hogendorn <jhogendorn@gmail.com>
2013-10-20 01:21:18 +1000

    A person on IRC said this was a good idea. what could go wrong? :)

--- api.rb
+++ api.rb
@@ -1,15 +1,19 @@
 require 'sinatra/base'
 
-configure do
-  mime_type :text, 'text/plain'
-end
-
 class Api < Sinatra::Base
+
+  configure do
+    set :views, 'api/views'
+    mime_type :text, 'text/plain'
+  end
+
   get '/api/heartbeat' do
     'True'
   end
+
   get '/api/scripts/clone' do
     content_type :text
-    liquid :clone, :views => 'api/views', :locals => { :env => ENV.to_hash }
+    liquid :clone, :locals => { :env => ENV.to_hash }
   end
+
 end
